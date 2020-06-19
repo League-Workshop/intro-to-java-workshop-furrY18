59850615aa197e914e5b9a852bbdb860ee8deb6f
root <root@67201097ee49>
2020-06-19 21:51:50 +0000

    worked on dragon fight, world domination, and candy crush.

--- src/section3/CandyCrush.java
+++ src/section3/CandyCrush.java
@@ -6,11 +6,10 @@ public class CandyCrush {
 
 	public static void main(String[] args) {
 		// 1. Using a pop-up, ask the user who they don't like
-		JOptionPane.showMessageDialog(what can you not stand? message);
+		JOptionPane.showMessageDialog(null, "wh0 can you not stand?");
 
-		// 2. Change the next line so that the pop-up will tell them they have a crush on that person
-		JOptionPane.showMessageDialog(null, "You totally have a crush on Java!");
 		
+		 JOptionPane.showMessageDialog(null,"you totally have a crush on ");
 		// 3. Ask the user for the name of their best friend
 
 		// 4. Tell them their best friend is as sweet as candy
